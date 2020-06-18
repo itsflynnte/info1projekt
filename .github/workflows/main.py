a5e54ffd40450c5ef2c94db1c32e57b9ffd450ee
itsflynnte <66907695+itsflynnte@users.noreply.github.com>
2020-06-18 16:52:18 +0200

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,1 +1,67 @@
-Hallo was geht
+import pygame
+
+pygame.display.set_caption("Mill by Jenson Schick and Mika Rasmussen")
+
+window_width = 200
+window_length = 200
+pygame.font(init()
+win = pygame.display.set_mode((window_width, window_length))
+
+
+
+
+#Forend + User Interface
+
+
+
+def startscreen():
+  win.fill(255,255,255)
+  draw_text_middle("Druecken Sie eine Taste, um das Spiel zu starten", 60, (0,0,0),win)
+  font = pygame.font.SysFont("arial", size, bold = true)
+  
+                     
+
+  
+def game_area():
+  
+
+
+
+def coordinates():
+
+
+def scoreboard():
+
+
+def stones():
+    def white_stone():
+
+    def black_stone():
+
+
+# Backend
+def movement(forward, backward):
+    forward =
+    backward =
+
+
+def beat():         #schlagen
+
+
+def stone_path():   #Stein weg
+
+
+def three_stones():  # jump
+
+
+def winning():
+
+
+def game_over():
+
+
+
+pygame.display.update()
+
+
+
